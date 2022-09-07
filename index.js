5a6562ad14c3e17641af01ed48746622d0b56f2e
Edwin3002 <vargas11742@gmail.com>
2022-09-07 17:21:32 -0500

    cors

--- index.js
+++ index.js
@@ -5,8 +5,8 @@ import appRoutes from './src/routes/app.routes.js';
 import taskRoutes from './src/routes/task.routes.js';
 
 const app = express();
-app.use(express.json());
 app.use(cors());
+app.use(express.json());
 
 //routes
 
